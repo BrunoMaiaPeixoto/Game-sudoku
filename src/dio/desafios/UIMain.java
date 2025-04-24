e749c50fad85957126acd73a72147d3529768717
BrunoMaiaPeixoto <brunomaiapeixoto@hotmail.com>
2025-04-23 23:32:54 -0300

    Aplicando a interface grafica usando swing

--- src/dio/desafios/UIMain.java
+++ src/dio/desafios/UIMain.java
@@ -2,18 +2,21 @@ package dio.desafios;
 
 import dio.desafios.ui.custom.frame.MainFrame;
 import dio.desafios.ui.custom.panel.MainPanel;
+import dio.desafios.ui.custom.screen.MainScreen;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UIMain {
 
     public static void main(String[] args) {
-        var dimension = new Dimension(600, 600);
-        JPanel mainPanel = new MainPanel(dimension);
-        JFrame mainFrame = new MainFrame(dimension, mainPanel);
-        mainFrame.revalidate();
-        mainFrame.repaint();
-    }
+        final var gameConfig = Stream.of(args)
+       .collect(Collectors.toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
 
+
+        var mainsScreen = new MainScreen(gameConfig);
+        mainsScreen.buildMainScreen();
+}
 }
